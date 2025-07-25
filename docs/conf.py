00625aaa1d49fc7153ec69b32ff9ea2d241d0da3
Akshay Shankar <sakshays.2000@gmail.com>
2025-07-25 19:19:15 +0200

    construct docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,10 +9,10 @@ import os
 
 # -- General configuration ------------------------------------------------
 
-needs_sphinx = "8.1"
+needs_sphinx = "1.3"
 
 # Sphinx extension module names and templates location
-sys.path.append(os.path.abspath("_extensions"))
+sys.path.append(os.path.abspath("godot-docs/_extensions"))
 extensions = [
     "sphinx_tabs.tabs",
     "notfound.extension",
@@ -29,6 +29,34 @@ sphinx_tabs_nowarn = True
 # Disable collapsing tabs for codeblocks.
 sphinx_tabs_disable_tab_closing = True
 
+# Custom 4O4 page HTML template.
+# https://github.com/readthedocs/sphinx-notfound-page
+notfound_context = {
+    "title": "Page not found",
+    "body": """
+        <h1>Page not found</h1>
+        <p>
+            Sorry, we couldn't find that page. It may have been renamed or removed
+            in the version of the documentation you're currently browsing.
+        </p>
+        <p>
+            If you're currently browsing the
+            <em>latest</em> version of the documentation, try browsing the
+            <a href="/en/stable/"><em>stable</em> version of the documentation</a>.
+        </p>
+        <p>
+            Alternatively, use the
+            <a href="#" onclick="$('#rtd-search-form [name=\\'q\\']').focus()">Search docs</a>
+            box on the left or <a href="/">go to the homepage</a>.
+        </p>
+    """,
+}
+
+# General information about the project
+project = "GDWiiInput"
+copyright = "2025-present Akshay Shankar and GDWiiInput contributors (CC by 3.0)"
+author = "Akshay Shankar and GDWiiInput contributors"
+
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
@@ -40,7 +68,6 @@ if not on_rtd:
 
 # Specify the site name for the Open Graph extension.
 ogp_site_name = "GDWiiInput documentation"
-ogp_social_cards = {"enable": False}
 
 if not os.getenv("SPHINX_NO_GDSCRIPT"):
     extensions.append("gdscript")
@@ -48,7 +75,7 @@ if not os.getenv("SPHINX_NO_GDSCRIPT"):
 if not os.getenv("SPHINX_NO_DESCRIPTIONS"):
     extensions.append("godot_descriptions")
 
-templates_path = ["_templates"]
+templates_path = ["_templates", "godot-docs/_templates/breadcrumbs.html"]
 
 # You can specify multiple suffix as a list of string: ['.rst', '.md']
 source_suffix = ".rst"
@@ -57,14 +84,12 @@ source_encoding = "utf-8-sig"
 # The master toctree document
 master_doc = "index"
 
-# General information about the project
-project = "GDWiiInput"
-copyright = "2025-present Akshay Shankar and the GDWiiInput contributors (CC BY 3.0)"
-author = "Akshay Shankar and the GDWiiInput contributors"
-
 # Version info for the project, acts as replacement for |version| and |release|
 # The short X.Y version
-version = os.getenv("READTHEDOCS_VERSION", "latest")
+version = os.getenv("READTHEDOCS_VERSION_NAME", "latest")
+if version.startswith("release/"):
+    version = version[8:]
+
 # The full version, including alpha/beta/rc tags
 release = version
 
@@ -78,20 +103,7 @@ if env_tags is not None:
 # Language / i18n
 
 supported_languages = {
-    "en": "Godot Engine %s documentation in English",
-    "de": "Godot Engine %s Dokumentation auf Deutsch",
-    "es": "Documentación de Godot Engine %s en español",
-    "fr": "Documentation de Godot Engine %s en français",
-    "fi": "Godot Engine %s dokumentaatio suomeksi",
-    "it": "Godot Engine %s documentazione in italiano",
-    "ja": "Godot Engine %sの日本語のドキュメント",
-    "ko": "Godot Engine %s 문서 (한국어)",
-    "pl": "Dokumentacja Godot Engine %s w języku polskim",
-    "pt_BR": "Documentação da Godot Engine %s em Português Brasileiro",
-    "ru": "Документация Godot Engine %s на русском языке",
-    "uk": "Документація до Godot Engine %s українською мовою",
-    "zh_CN": "Godot Engine %s 简体中文文档",
-    "zh_TW": "Godot Engine %s 正體中文 (台灣) 文件",
+    "en": "GDWiiInput %s documentation in English",
 }
 
 # RTD normalized their language codes to ll-cc (e.g. zh-cn),
@@ -111,9 +123,17 @@ if not language in supported_languages.keys():
     language = "en"
 
 is_i18n = tags.has("i18n")  # noqa: F821
+if is_i18n:
+    raise RuntimeError("i18n is currently not supported.")
+
 print("Build language: {}, i18n tag: {}".format(language, is_i18n))
 
-exclude_patterns = [".*", "**/.*", "_build", "_tools"]
+exclude_patterns = [
+    # Don't build the build folder accidentally.
+    "_build",
+    # Don't build whatever godot-docs has to offer.
+    "godot-docs/**",
+]
 
 # fmt: off
 # These imports should *not* be moved to the start of the file,
@@ -134,6 +154,7 @@ highlight_language = "gdscript"
 # -- Options for HTML output ----------------------------------------------
 
 html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 if on_rtd:
     using_rtd_theme = True
 
@@ -141,34 +162,38 @@ if on_rtd:
 html_theme_options = {
     # if we have a html_logo below, this shows /only/ the logo with no title text
     "logo_only": True,
-    # Collapse navigation (False makes it tree-like)
+    # Collapse navigation (False makes it tree-like, aka navigation items are 'expanded' by default)
+    # Header collapse cannot be disabled yet, see https://github.com/readthedocs/sphinx_rtd_theme/issues/455
     "collapse_navigation": False,
-    # Remove version and language picker beneath the title
-    "version_selector": False,
-    "language_selector": False,
-    # Set Flyout menu to attached
-    "flyout_display": "attached",
+    # Max depth of navigation in the sidebar.
+    "navigation_depth": 2,
+    # Hide the documentation version name/number under the logo
+    "display_version": False,
 }
 
 html_title = supported_languages[language] % ("(" + version + ")")
 
-# Edit on GitHub options: https://docs.readthedocs.io/en/latest/guides/edit-source-links-sphinx.html
+# VCS options: https://docs.readthedocs.io/en/latest/vcs.html#github
 html_context = {
     "display_github": not is_i18n,  # Integrate GitHub
-    "github_user": "20akshay00",  # Username
-    "github_repo": "godot-wii-input",  # Repo name
-    "github_version": "master",  # Version
-    "conf_py_path": "/",  # Path in the checkout to the docs root
+    "github_user": "ivorforce",  # Username
+    "github_repo": "GDWiiInput",  # Repo name
+    "github_version": "main",  # Version
+    "conf_py_path": "/docs/",  # Path in the checkout to the docs root
+    "godot_inject_language_links": True,
+    "godot_docs_supported_languages": list(supported_languages.keys()),
     "godot_docs_title": supported_languages[language],
-    "godot_docs_basepath": "https://docs.godotengine.org/",
+    "godot_docs_basepath": "https://GDWiiInput.readthedocs.io/",
     "godot_docs_suffix": ".html",
+    "godot_default_lang": "en",
+    "godot_canonical_version": "stable",
     # Distinguish local development website from production website.
     # This prevents people from looking for changes on the production website after making local changes :)
     "godot_title_prefix": "" if on_rtd else "(DEV) ",
     # Set this to `True` when in the `latest` branch to clearly indicate to the reader
     # that they are not reading the `stable` documentation.
-    "godot_is_latest": True,
-    "godot_version": "4.4",
+    "godot_is_latest": version == "latest",
+    "godot_version": version,
     # Enables a banner that displays the up-to-date status of each article.
     "godot_show_article_status": True,
     # Display user-contributed notes at the bottom of pages that don't have `:allow_comments: False` at the top.
@@ -178,25 +203,23 @@ html_context = {
 html_logo = "img/docs_logo.svg"
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ["_static"]
+html_static_path = ["godot-docs/_static"]
 
-html_extra_path = ["robots.txt"]
+html_extra_path = ["robots.txt", "google7747f782ed971a55.html"]
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (e.g. https://...)
 html_css_files = [
-    "css/custom.css",
+    "css/algolia.css",
+    "https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css",
+    "css/custom.css?10",  # Increment the number at the end when the file changes to bust the cache.
 ]
 
 if not on_rtd:
     html_css_files.append("css/dev.css")
 
 html_js_files = [
-    "js/custom.js",
-    (
-        "https://plausible.godot.foundation/js/script.file-downloads.outbound-links.js",
-        {"defer": "defer", "data-domain": "godotengine.org"},
-    ),
+    "js/custom.js?7",  # Increment the number at the end when the file changes to bust the cache.
 ]
 
 # Output file base name for HTML help builder
@@ -217,7 +240,7 @@ latex_documents = [
         master_doc,
         "GDWiiInput.tex",
         "GDWiiInput Documentation",
-        "Akshay Shankar and the GDWiiInput contributors",
+        "Akshay Shankar and GDWiiInput contributors",
         "manual",
     ),
 ]
@@ -228,92 +251,6 @@ latex_documents = [
 linkcheck_anchors = False
 
 linkcheck_timeout = 10
-
-# -- Custom Sphinx roles for UI -------------------------------------------
-
-rst_prolog = """
-.. role:: button
-   :class: role-button role-ui
-
-.. role:: menu
-    :class: role-menu role-ui
-
-.. role:: inspector
-    :class: role-ui
-
-.. role:: ui
-    :class: role-ui
-
-"""
-
-# -- I18n settings --------------------------------------------------------
-
-# Godot localization is handled via https://github.com/godotengine/godot-docs-l10n
-# where the main docs repo is a submodule. Therefore the translated material is
-# actually in the parent folder of this conf.py, hence the "../".
-
-locale_dirs = ["../sphinx/po/"]
-gettext_compact = False
-
-# We want to host the localized images in godot-docs-l10n, but Sphinx does not provide
-# the necessary feature to do so. `figure_language_filename` has `{root}` and `{path}`,
-# but they resolve to (host) absolute paths, so we can't use them as is to access "../".
-# However, Python is glorious and lets us redefine Sphinx's internal method that handles
-# `figure_language_filename`, so we do our own post-processing to fix the absolute path
-# and point to the parallel folder structure in godot-docs-l10n.
-# Note: Sphinx's handling of `figure_language_filename` may change in the future, monitor
-# https://github.com/sphinx-doc/sphinx/issues/7768 to see what would be relevant for us.
-figure_language_filename = "{root}.{language}{ext}"
-
-cwd = os.getcwd()
-
-sphinx_original_get_image_filename_for_language = (
-    sphinx.util.i18n.get_image_filename_for_language
-)
-
-
-def godot_get_image_filename_for_language(filename, env):
-    """
-    Hack the absolute path returned by Sphinx based on `figure_language_filename`
-    to insert our `../images` relative path to godot-docs-l10n's images folder,
-    which mirrors the folder structure of the docs repository.
-    The returned string should also be absolute so that `os.path.exists` can properly
-    resolve it when trying to concatenate with the original doc folder.
-    """
-    path = sphinx_original_get_image_filename_for_language(filename, env)
-    path = os.path.abspath(os.path.join("../images/", os.path.relpath(path, cwd)))
-    return path
-
-
-sphinx.util.i18n.get_image_filename_for_language = godot_get_image_filename_for_language
-
-# Similar story for the localized class reference, it's out of tree and there doesn't
-# seem to be an easy way for us to tweak the toctree to take this into account.
-# So we're deleting the existing class reference and adding a symlink instead...
-if is_i18n and os.path.exists("../classes/" + language):
-    import shutil
-
-    if os.path.islink("classes"):  # Previously made symlink.
-        os.unlink("classes")
-    else:
-        shutil.rmtree("classes")
-
-    os.symlink("../classes/" + language, "classes")
-
-# Couldn't find a way to retrieve variables nor do advanced string
-# concat from reST, so had to hardcode this in the "epilog" added to
-# all pages. This is used in index.rst to display the Weblate badge.
-# On English pages, the badge points to the language-neutral engage page.
-rst_epilog = """
-.. |weblate_widget| image:: https://hosted.weblate.org/widgets/godot-engine/{image_locale}/godot-docs/287x66-white.png
-    :alt: Translation status
-    :target: https://hosted.weblate.org/engage/godot-engine{target_locale}/?utm_source=widget
-    :width: 287
-    :height: 66
-""".format(
-    image_locale="-" if language == "en" else language,
-    target_locale="" if language == "en" else "/" + language,
-)
 
 # Needed so the table of contents is created for EPUB
 epub_tocscope = "includehidden"
