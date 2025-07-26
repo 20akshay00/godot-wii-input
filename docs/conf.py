5f74e18d085f98fff4376cb488664ad8900a45de
Akshay <sakshays.2000@gmail.com>
2025-07-26 11:41:51 +0200

    switch to custom css

--- docs/conf.py
+++ docs/conf.py
@@ -203,9 +203,9 @@ html_context = {
 html_logo = "images/GDWiiInputDocs.svg"
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ["godot-docs/_static"]
+html_static_path = ["_static"]
 
-html_extra_path = ["robots.txt", "google7747f782ed971a55.html"]
+html_extra_path = ["robots.txt"]
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (e.g. https://...)
