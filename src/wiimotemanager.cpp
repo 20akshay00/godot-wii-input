1a4ba064226544357b32dee65cac6b1710fadadf
Akshay <sakshays.2000@gmail.com>
2025-07-26 20:23:51 +0200

    remove godot namespace

--- src/wiimotemanager.cpp
+++ src/wiimotemanager.cpp
@@ -9,26 +9,20 @@
 
 #include "debug.h"
 
-using namespace godot;
-
-void notification()
-{
-}
-
 void WiimoteManager::_bind_methods()
 {
-    ClassDB::bind_method(D_METHOD("set_max_wiimotes", "max"), &WiimoteManager::set_max_wiimotes);
-    ClassDB::bind_method(D_METHOD("get_max_wiimotes"), &WiimoteManager::get_max_wiimotes);
-    ADD_PROPERTY(PropertyInfo(Variant::INT, "max_wiimotes"), "set_max_wiimotes", "get_max_wiimotes");
+    godot::ClassDB::bind_method(godot::D_METHOD("set_max_wiimotes", "max"), &WiimoteManager::set_max_wiimotes);
+    godot::ClassDB::bind_method(godot::D_METHOD("get_max_wiimotes"), &WiimoteManager::get_max_wiimotes);
+    ADD_PROPERTY(godot::PropertyInfo(godot::Variant::INT, "max_wiimotes"), "set_max_wiimotes", "get_max_wiimotes");
 
-    ClassDB::bind_method(D_METHOD("connect_wiimotes"), &WiimoteManager::connect_wiimotes);
-    ClassDB::bind_method(D_METHOD("finalize_connection"), &WiimoteManager::finalize_connection);
-    ClassDB::bind_method(D_METHOD("disconnect_wiimotes"), &WiimoteManager::disconnect_wiimotes);
-    ClassDB::bind_method(D_METHOD("get_connected_wiimotes"), &WiimoteManager::get_connected_wiimotes);
+    godot::ClassDB::bind_method(godot::D_METHOD("connect_wiimotes"), &WiimoteManager::connect_wiimotes);
+    godot::ClassDB::bind_method(godot::D_METHOD("finalize_connection"), &WiimoteManager::finalize_connection);
+    godot::ClassDB::bind_method(godot::D_METHOD("disconnect_wiimotes"), &WiimoteManager::disconnect_wiimotes);
+    godot::ClassDB::bind_method(godot::D_METHOD("get_connected_wiimotes"), &WiimoteManager::get_connected_wiimotes);
 
-    ClassDB::bind_method(D_METHOD("start_polling"), &WiimoteManager::enable_polling);
-    ClassDB::bind_method(D_METHOD("stop_polling"), &WiimoteManager::disable_polling);
-    ClassDB::bind_method(D_METHOD("poll"), &WiimoteManager::poll);
+    godot::ClassDB::bind_method(godot::D_METHOD("start_polling"), &WiimoteManager::enable_polling);
+    godot::ClassDB::bind_method(godot::D_METHOD("stop_polling"), &WiimoteManager::disable_polling);
+    godot::ClassDB::bind_method(godot::D_METHOD("poll"), &WiimoteManager::poll);
 }
 
 WiimoteManager *WiimoteManager::singleton = nullptr;
@@ -49,10 +43,10 @@ void WiimoteManager::set_max_wiimotes(int max)
 {
     if (num_connected >= 0)
     {
-        UtilityFunctions::print("Cannot change max_wiimotes while connected!");
+        godot::UtilityFunctions::print("Cannot change max_wiimotes while connected!");
         return;
     }
-    max_wiimotes = MAX(1, MIN(max, 16)); // kind of an arbitrary limit
+    max_wiimotes = godot::MAX(1, godot::MIN(max, 16)); // kind of an arbitrary limit
 }
 
 int WiimoteManager::get_max_wiimotes() const
@@ -64,7 +58,7 @@ void WiimoteManager::connect_wiimotes()
 {
     if (num_connected >= 0)
     {
-        UtilityFunctions::print("Cannot connect during runtime!");
+        godot::UtilityFunctions::print("Cannot connect during runtime!");
         return;
     }
 
@@ -74,7 +68,7 @@ void WiimoteManager::connect_wiimotes()
     int found = wiiuse_find(wiimotes, max_wiimotes, 5);
     if (found <= 0)
     {
-        UtilityFunctions::print("No Wiimotes found.");
+        godot::UtilityFunctions::print("No Wiimotes found.");
         wiiuse_cleanup(wiimotes, max_wiimotes);
         wiimotes = nullptr;
         return;
@@ -83,17 +77,17 @@ void WiimoteManager::connect_wiimotes()
     num_connected = wiiuse_connect(wiimotes, max_wiimotes);
     if (num_connected <= 0)
     {
-        UtilityFunctions::print("Found ", found, " but could not connect to any.");
+        godot::UtilityFunctions::print("Found ", found, " but could not connect to any.");
         wiiuse_cleanup(wiimotes, max_wiimotes);
         wiimotes = nullptr;
         return;
     }
 
-    UtilityFunctions::print("Wiimotes found: ", found, ", connected: ", num_connected);
+    godot::UtilityFunctions::print("Wiimotes found: ", found, ", connected: ", num_connected);
     return;
 }
 
-TypedArray<GDWiimote> WiimoteManager::finalize_connection()
+godot::TypedArray<GDWiimote> WiimoteManager::finalize_connection()
 {
     // Wrap each connected wiimote
     gdwiimotes.clear();
@@ -103,7 +97,7 @@ TypedArray<GDWiimote> WiimoteManager::finalize_connection()
         if (!wiimotes[i] || !WIIMOTE_IS_CONNECTED(wiimotes[i]))
             continue;
 
-        Ref<GDWiimote> gdwm = memnew(GDWiimote(wiimotes[i], i));
+        godot::Ref<GDWiimote> gdwm = memnew(GDWiimote(wiimotes[i], i));
         gdwiimotes.push_back(gdwm);
 
         // doesn't make sense to use gdwiimote.set_leds here since it requires a GDArray
@@ -152,10 +146,10 @@ void WiimoteManager::disconnect_wiimotes()
 
 void WiimoteManager::enable_polling()
 {
-    SceneTree *tree = Object::cast_to<SceneTree>(Engine::get_singleton()->get_main_loop());
+    godot::SceneTree *tree = Object::cast_to<godot::SceneTree>(godot::Engine::get_singleton()->get_main_loop());
     if (tree)
     {
-        Callable cb = callable_mp(this, &WiimoteManager::poll);
+        godot::Callable cb = callable_mp(this, &WiimoteManager::poll);
 
         if (tree->is_connected("process_frame", cb))
         {
@@ -170,10 +164,10 @@ void WiimoteManager::enable_polling()
 
 void WiimoteManager::disable_polling()
 {
-    SceneTree *tree = Object::cast_to<SceneTree>(Engine::get_singleton()->get_main_loop());
+    godot::SceneTree *tree = Object::cast_to<godot::SceneTree>(godot::Engine::get_singleton()->get_main_loop());
     if (tree)
     {
-        Callable cb = callable_mp(this, &WiimoteManager::poll);
+        godot::Callable cb = callable_mp(this, &WiimoteManager::poll);
 
         if (tree->is_connected("process_frame", cb))
         {
@@ -196,7 +190,7 @@ void WiimoteManager::poll()
     {
         for (int i = 0; i < gdwiimotes.size(); i++)
         {
-            Ref<GDWiimote> gdwm = gdwiimotes[i];
+            godot::Ref<GDWiimote> gdwm = gdwiimotes[i];
             if (!gdwm.is_valid())
                 continue;
 
